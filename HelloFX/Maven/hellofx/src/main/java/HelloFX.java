654616e67eb6d95fddc88392c8cb69e114b39a39
pgschulz <50008367+pgschulz@users.noreply.github.com>
2024-02-02 10:07:54 +0100

    Update HelloFX.java
    Custom HelloFX Application to test 308-Redirect behaviour

--- HelloFX/Maven/hellofx/src/main/java/HelloFX.java
+++ HelloFX/Maven/hellofx/src/main/java/HelloFX.java
@@ -1,23 +1,54 @@
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.concurrent.Worker.State;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.PopupFeatures;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.util.Callback;
+
 
 public class HelloFX extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+	public static void main(String[] args) {
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch();
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX WebView Example");
+
+        WebView webView = new WebView();
+
+		webView.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>()
+        {
+            @Override
+            public void changed(final ObservableValue bObservable, final Worker.State oldValue, final Worker.State newValue)
+            {
+                if (((webView.getEngine() != null) && (webView.getEngine().getLoadWorker().getException() != null)) && (newValue == State.FAILED)) {
+                    System.out.println(webView.getEngine().getLoadWorker().getException());
+                }
+            }
+		});
+		
+		webView.getEngine().load("https://iconclass.org/20(%2B0)");
+		// webView.getEngine().load("https://www.getty.edu/research/tools/vocabularies/ulan");
+		// webView.getEngine().load(index.html);
+		//webView.getEngine().load("https://webdbg.com/ua.aspx");
+
+        VBox vBox = new VBox(webView);
+        Scene scene = new Scene(vBox, 960, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 
 }
